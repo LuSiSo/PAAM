2d4b6881f9f53053d9bb83333715cb839b7eba33
LuSiSo <silveriodesousalucas6@gmail.com>
2026-05-20 15:36:14 -0300

    Update script.js

--- Execessão/exercício 1/script.js
+++ Execessão/exercício 1/script.js
@@ -1,1 +1,35 @@
+document.getElementById("formCadastro").addEventListener("submit", function (e) {
+  e.preventDefault();
 
+  const nome = document.getElementById("nome").value;
+  const email = document.getElementById("email").value;
+  const idade = Number(document.getElementById("idade").value);
+  const senha = document.getElementById("senha").value;
+
+  const msg = document.getElementById("mensagem");
+
+  try {
+    if (!nome || nome.length < 3) {
+      throw "Nome inválido (mínimo 3 caracteres).";
+    }
+
+    if (!email.includes("@")) {
+      throw "E-mail inválido.";
+    }
+
+    if (!idade || idade < 18) {
+      throw "Idade inválida (mínimo 18 anos).";
+    }
+
+    if (!senha || senha.length < 6) {
+      throw "Senha deve ter no mínimo 6 caracteres.";
+    }
+
+    msg.style.color = "green";
+    msg.textContent = "Cadastro realizado com sucesso!";
+  } 
+  catch (erro) {
+    msg.style.color = "red";
+    msg.textContent = erro;
+  }
+});
